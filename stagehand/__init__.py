e8a19478728e724b88680ae37cab2ddc62bebbb2
Eric Smaxwill <eric.smaxwill@lattice.com>
2023-08-01 12:36:56 -0500

    Add tests for tag values

--- stagehand/__init__.py
+++ stagehand/__init__.py
@@ -1,2 +1,3 @@
 from .client import Client, Fields, Filter, Owner
+from .types import LabelName, EntityReference
 from .models import Entity
--- stagehand/types.py
+++ stagehand/types.py
@@ -1,12 +1,10 @@
+import re
 import typing
-from typing_extensions import Annotated
 
 import pydantic
 import pydantic_core
 
-from . import validators
 
-# Name = Annotated[
 Name = pydantic.constr(
     min_length=1,
     max_length=63,
@@ -15,51 +13,37 @@ Name = pydantic.constr(
     pattern=r"^[a-z0-9A-Z\.\-_:]+$",
 )
 
+
+AnnotationName = pydantic.constr(
+    strict=True, max_length=63, min_length=1, pattern=r"[A-Za-z0-9\-\./]+"
+)
+AnnotationValue = str
+AnnotationDict = typing.Dict[AnnotationName, AnnotationValue]
+
+
 LabelValue = pydantic.constr(
     strict=True, strip_whitespace=False, pattern=r"^[a-z0-9A-Z\.\-_:]{1,63}$"
 )
-TagValue = pydantic.constr(
-    strict=True, strip_whitespace=False, pattern=r"[a-z0-9:+#\-]{1,63}"
-)
-
-
-class EntityReference(pydantic.BaseModel):
-    name: Name
-    kind: Name | None = None
-    namespace: Name | None = None
-
-    @classmethod
-    def format(cls, kind: str | None, namespace: str | None, name: str) -> str:
-        return (
-            f"{kind + ':' if kind else ''}{namespace + '/' if namespace else ''}{name}"
-        )
-
-    @classmethod
-    def parse(cls, s: str) -> "EntityReference":
-        """Parses a string into an EntityReference class"""
-        return cls(**validators.validate_entity_reference(s))
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls,
-        __source: type[pydantic.BaseModel],
-        __handler: pydantic.GetCoreSchemaHandler,
-    ) -> pydantic_core.CoreSchema:
-        return pydantic_core.core_schema.no_info_before_validator_function(
-            function=validators.validate_entity_reference, schema=__handler(cls)
-        )
-
-    def __str__(self) -> str:
-        """Get the string representation of the reference"""
-        return self.format(self.kind, self.namespace, self.name).lower()
-
-    def __eq__(self, __value: object) -> bool:
-        return __value == str(self)
 
 
 class LabelName(pydantic.BaseModel):
     prefix: str | None = None
     name: str
+
+    _label_key: typing.ClassVar[re.Pattern] = re.compile(
+        r"^(?:(?P<prefix>(?:(?!-)[A-Za-z0-9\-]{1,253}(?<!-)\.)+(?:[A-Za-z]{2,6}))/)?(?P<name>[a-zA-Z\-\.]{1,63})$"
+    )
+
+    @classmethod
+    def parse_label(cls, v: str) -> dict:
+        if isinstance(v, dict):
+            return v
+
+        match = cls._label_key.match(v)
+        if match is None:
+            return None
+
+        return match.groupdict()
 
     @classmethod
     def format(cls, prefix, name) -> str:
@@ -70,7 +54,7 @@ class LabelName(pydantic.BaseModel):
         cls, source_type: typing.Any, handler: pydantic.GetCoreSchemaHandler
     ) -> pydantic_core.CoreSchema:
         return pydantic_core.core_schema.no_info_before_validator_function(
-            function=validators.parse_label, schema=handler(cls)
+            function=cls.parse_label, schema=handler(cls)
         )
 
     def __str__(self) -> str:
@@ -83,11 +67,65 @@ class LabelName(pydantic.BaseModel):
         return __value == str(self)
 
 
-AnnotationName = pydantic.constr(
-    strict=True, max_length=63, min_length=1, pattern=r"[A-Za-z0-9\-\./]+"
-)
-AnnotationValue = str
-AnnotationDict = typing.Dict[AnnotationName, AnnotationValue]
-
 LabelDict = typing.Dict[LabelName, LabelValue]
+
+
+class EntityReference(pydantic.BaseModel):
+    name: Name
+    kind: Name | None = None
+    namespace: Name | None = None
+
+    _entity_reference: typing.ClassVar[re.Pattern] = re.compile(
+        r"^(?:(?P<kind>[a-zA-Z]{1,63}):)?(?:(?P<namespace>[a-zA-Z\-]{1,63})/)?(?P<name>[a-zA-Z\-0-9]{1,63})$"
+    )
+
+    @classmethod
+    def validate_entity_reference(cls, v: str) -> dict:
+        if isinstance(v, dict):
+            return v
+
+        default = {"name": None, "namespace": None, "kind": None}
+
+        match = cls._entity_reference.match(v)
+        if match is None:
+            return default
+
+        return match.groupdict()
+
+    @classmethod
+    def format(cls, kind: str | None, namespace: str | None, name: str) -> str:
+        return (
+            f"{kind + ':' if kind else ''}{namespace + '/' if namespace else ''}{name}"
+        )
+
+    @classmethod
+    def parse(cls, s: str) -> "EntityReference":
+        """Parses a string into an EntityReference class"""
+        return cls(**cls.validate_entity_reference(s))
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        __source: type[pydantic.BaseModel],
+        __handler: pydantic.GetCoreSchemaHandler,
+    ) -> pydantic_core.CoreSchema:
+        return pydantic_core.core_schema.no_info_before_validator_function(
+            function=cls.validate_entity_reference, schema=__handler(cls)
+        )
+
+    def __str__(self) -> str:
+        """Get the string representation of the reference"""
+        return self.format(self.kind, self.namespace, self.name).lower()
+
+    def __eq__(self, __value: object) -> bool:
+        return __value == str(self)
+
+
+TagValue = pydantic.constr(
+    strict=True,
+    strip_whitespace=False,
+    min_length=1,
+    max_length=63,
+    pattern=r"^(?:(?:[a-z0-9:+#]+)-?)+$",
+)
 TagList = typing.List[TagValue]
